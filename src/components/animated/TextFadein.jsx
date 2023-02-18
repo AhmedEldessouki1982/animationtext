1a0d1e16f6acdc98dd02e892991160f1ab56fae8
AhmedEldessouki1982 <86360415+AhmedEldessouki1982@users.noreply.github.com>
2023-02-18 17:25:04 +0400

    animation forms, 1st commit

---
+++ src/components/animated/TextFadein.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../css/textFadeIn.css'
+
+function TextFadein() {
+  return (
+    <div className='container'>
+        {/*upper name value */}
+        <div className='name'>
+            <h1>ahmed</h1>
+        </div>
+
+        {/*lower name value*/}
+        <div className='name'>
+            <h1>eldessouki</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TextFadein
